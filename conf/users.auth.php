82eb7043c8bd36dc08c61fd1fc561a7fd4c14195
kontakt <kontakt@pomorze.hackerspace.pl>
2020-09-06 23:08:03 +0200

    backup @ Sun Sep  6 21:08:03 UTC 2020

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,separated
+
+
+yasiu:$1$nAcCOwaU$SK5YYjCgorL.I1JTPi.Cr1:Marcin Jasiukowicz:contact@yasiu.pl:admin,user
